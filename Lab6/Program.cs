987bff7a14dd414723759bc297b4eb7dc42c676d
kyle-nedbal <kylenedbal88@gmail.com>
2020-04-27 14:49:32 -0400

    Lab 6

---
+++ Lab6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string userWantsToContinue;
+            int diceSides;
+
+            Console.Write("Welcome to the Grand Circus Casino! Roll the dice? (y/n): ");
+            string userWantsToStart = Console.ReadLine();
+            if (userWantsToStart.ToUpper() == "Y")
+            {
+
+                do
+                {
+                    Console.Write("How many sides should each dice have? ");
+                    diceSides = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("You rolled:");
+
+                    DiceRoll(diceSides);
+                    DiceRoll(diceSides);
+
+                    Console.Write("Roll again? (y/n): ");
+                    userWantsToContinue = Console.ReadLine();
+                } while (userWantsToContinue.ToUpper() == "Y");
+            }
+        }
+
+        public static int DiceRoll(int diceSides)
+        { 
+            var roll = new Random();
+            Console.WriteLine(roll.Next(1, (diceSides + 1)));
+            return diceSides;
+        }
+    }
+}
